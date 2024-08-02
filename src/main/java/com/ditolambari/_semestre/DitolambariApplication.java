596a09a24dd18fb01871290109b075409d1a1e58
lorennavitoria <lorennalovi@gmail.com>
2024-08-02 15:59:02 -0400

    meu primeiro commit

---
+++ src/main/java/com/ditolambari/_semestre/DitolambariApplication.java
@@ -0,0 +1,13 @@
+package com.ditolambari._semestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DitolambariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DitolambariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ditolambari/_semestre/DitolambariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ditolambari._semestre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DitolambariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
