f61c49339284371bdedc0e2ae864ea45f04add04
lorennavitoria <lorennalovi@gmail.com>
2024-08-02 16:44:01 -0400

    Adicionado telas html ao sistema

---
+++ src/main/java/com/ditolambari/_semestre/controller/testeController.java
@@ -0,0 +1,29 @@
+package com.ditolambari._semestre.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/teste")
+public class testeController { 
+
+	@RequestMapping("/index")
+	public String telaIndex() {
+		return "index.html";
+	}
+	
+	@RequestMapping("/contato")
+	public String telaContato() {
+		return "contato.html";
+	}
+	
+	@RequestMapping("/receitas")
+	public String telaReceitas() {
+		return "receitas.html";
+	}
+	
+	@RequestMapping("/reservas")
+	public String telaSobre() {
+		return "reservas.html";
+	}
+}
